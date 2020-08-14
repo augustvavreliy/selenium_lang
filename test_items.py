ffcac3fad2506fcb93de0b560df54586329b71c3
augustvareliy <augustvavreliy@gmail.com>
2020-08-14 21:55:02 +0300

    first commit

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_add_items(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element_by_xpath("//button[@type='submit']")
+    assert button
